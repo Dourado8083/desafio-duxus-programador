8bdb3283033a58561d973da57e9e314ba7967300
Gustavo Dourado <gustavo.dourado.br@gmail.com>
2026-06-12 14:09:39 -0300

    (fix)correcao do tipo do repositorio, estava gerando erro no swagger

--- src/main/java/br/com/duxusdesafio/repository/IntegranteRepository.java
+++ src/main/java/br/com/duxusdesafio/repository/IntegranteRepository.java
@@ -3,5 +3,5 @@ package br.com.duxusdesafio.repository;
 import br.com.duxusdesafio.model.Integrante;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IntegranteRepository extends JpaRepository<Integrante, Integer> {
+public interface IntegranteRepository extends JpaRepository<Integrante,Long> {
 }
