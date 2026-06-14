09cab65fac0e9d3b8372d4dec53d2ea9259cc5bd
Gustavo Dourado <gustavo.dourado.br@gmail.com>
2026-06-14 18:05:40 -0300

    (feature) adicionando e criando as conexao com  axios no projeto e mudando a porta do Cors para conseguir acessar as requisicoes

--- src/main/java/br/com/duxusdesafio/config/CorsConfigure.java
+++ src/main/java/br/com/duxusdesafio/config/CorsConfigure.java
@@ -9,7 +9,7 @@ public class CorsConfigure implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
